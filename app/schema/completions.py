763f7dad872c64ba48ce9f996f38d39ca6846958
ProsD <alex.prosdocimo@gmail.com>
2025-09-26 21:33:27 +0200

    Added Async Response for v1/completions

--- app/schema/completions.py
+++ app/schema/completions.py
@@ -17,6 +17,9 @@ class MessageObject(BaseModel):
 class CompletionsRequest(BaseModel):
     model: str = Field(description="Requested model to generate the completion")
     prompt: List[MessageObject] | str = Field(description="Prompt to start the generation, either a single message or an array of messages")
+    max_tokens: int | None = Field(description="Maximum number of tokens to generate")
+    temperature: float | None = Field(description="Temperature to use in the generation")
+    stream: bool | None = Field(description="Whether the generation should be streamed")
 
 class FinishReasonEnum(str, Enum):
     stop = "stop"
@@ -26,7 +29,7 @@ class FinishReasonEnum(str, Enum):
 class ChoiceObject(BaseModel):
     text: str = Field(description="Generated text for this choice")
     index: int = Field(description="Index of the choice, usually 0", default=0)
-    finish_reason: FinishReasonEnum = Field(description="Reason for completion finish. Must be either stop, length, or content_filter")
+    finish_reason: FinishReasonEnum | None = Field(description="Reason for completion finish. Must be either stop, length, or content_filter")
 
 class UsageObject(BaseModel):
     prompt_tokens: int = Field(description="Number of tokens used by the prompt (system prompt and messages). Defaults to 0 if the provider does not give usage info", default=0)
@@ -39,6 +42,14 @@ class CompletionsResponse(BaseModel):
     created: int = Field(description="Creation date as epoch timestamp", default=datetime.now().timestamp())
     model: str = Field(description="Name of model that generated the completion")
     choices: List[ChoiceObject] = Field(description="List of choice objects for this completion")
-    usage: UsageObject = Field(description="Usage object for this completion")
+    usage: UsageObject = Field(description="Usage object for this completion", default=UsageObject())
+
+class AsyncCompletionsResponse(BaseModel):
+    id: str = Field(description="ID of the completion request", default=f"cmpl-{uuid.uuid4()}")
+    object: str = Field(description="Type of object, usually text_completion", default="text_completion")
+    created: int = Field(description="Creation date as epoch timestamp", default=datetime.now().timestamp())
+    model: str = Field(description="Name of model that generated the completion")
+    choices: List[ChoiceObject] = Field(description="List of choice objects for this completion")
+
 
 
